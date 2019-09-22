198a754c4faceb2e86b3676321adc20bdaa7faf7
carlopisacane <doncarlopisacane@gmail.com>
2019-09-22 17:05:14 +0200

    added NavBar component and fontawesome in index.html

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import NavBar from './components/layout/NavBar'
 import './App.css';
 
 class App extends Component {
-  
+
 
   render() {
     return (
       <div className="App">
-        <h1>Hello from React</h1>
+        <NavBar/>
       </div>
     );
   }

3df0a1a72e293513ebe069d3f0ef476e31ef574f
carlopisacane <doncarlopisacane@gmail.com>
2019-09-26 18:00:23 +0200

    Moving Repos State To Context

--- src/App.js
+++ src/App.js
@@ -5,39 +5,14 @@
   import Search from './components/users/Search';
   import Alert from './components/layout/Alert';
   import About from './components/pages/About';
-  import User from './components/users/User'
-  import axios from 'axios';
+  import User from './components/users/User';
 
   import GithubState from './context/github/GithubState';
 
   import './App.css';
 
   const App = () => {
-    const [repos, setRepos] = useState([]);
-    const [loading, setLoading] = useState(false);
     const [alert, setAlert] = useState(null);
-
-    // Search Github users
-    
-
-    // Get single Github user
-    
-
-    // Get user repos
-    const getUserRepos = async (username) => {
-      setLoading(true);
-
-      const res = await axios.get(
-        `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
-          process.env.REACT_APP_GITHUB_CLIENT_ID
-        }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-      );
-
-      setRepos(res.data);
-      setLoading(false);
-    }
-
-    // Clear users from state
 
     // Set Alert
     const showAlert = (msg, type) => {
@@ -65,11 +40,7 @@
                   </Fragment>)}
                 />
                 <Route exact path='/about' component={About} />
-                <Route exact path='/user/:login' render={props => (
-                  <User 
-                    {...props} 
-                    getUserRepos={getUserRepos}
-                    repos={repos}                  />
+                <Route exact path='/user/:login' component={User} />
                 )}/>
               </Switch>
             </div>
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,14 +1,13 @@
 import React, { useEffect, Fragment, useContext } from 'react';
 import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types';
 import Repos from '../repos/Repos';
 import GithubContext from '../../context/github/githubContext';
 import { Link } from 'react-router-dom';
 
-const User = ({  getUserRepos, repos, match }) => {
+const User = ({ match }) => {
   const githubContext = useContext(GithubContext);
   
-  const { user, loading, getUser } = githubContext;
+  const { user, loading, getUser, repos, getUserRepos } = githubContext;
 
   useEffect(() => {
     getUser(match.params.login);
@@ -96,13 +95,5 @@ const User = ({  getUserRepos, repos, match }) => {
     </Fragment>
   );
 }
-
-User.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.array.isRequired,
-  repos: PropTypes.array.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.func.isRequired
-};
 
 export default User

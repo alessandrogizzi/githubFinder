ce5da24ae5ee5281db0f467ff7322e4c18293788
carlopisacane <doncarlopisacane@gmail.com>
2019-09-26 18:56:52 +0200

    Alert Context Workflow

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,16 +1,19 @@
 import React, { useState, useContext } from 'react';
-import PropTypes from 'prop-types';
 import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
 
-const Search = ({ setAlert }) => {
+
+const Search = () => {
   const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+
 
   const [text, setText] = useState('');
 
   const onSubmit = e => {
     e.preventDefault();
     if(text === '') {
-      setAlert('Please enter something', 'light')
+      alertContext.setAlert('Please enter something', 'light')
     } else {
       githubContext.searchUsers(text);
       setText('');
@@ -44,9 +47,5 @@ const Search = ({ setAlert }) => {
     </div>
   )
 }
-
-Search.propTypes = {
-  setAlert: PropTypes.func.isRequired
-};
 
 export default Search;
